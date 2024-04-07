71901f348c6a7c8ced1b3522078ef7e301a8d682
Alexander Kuznetsov <166259762+a-kuznetsov-mipt@users.noreply.github.com>
2024-04-07 18:26:36 +0300

    Qt application object creation moved to a separated class
    The class implements a method for the main window to show itself and a method for execution which is equal to QApplication.exec

--- bookkeeper/gui_client.py
+++ bookkeeper/gui_client.py
@@ -3,6 +3,7 @@
 чтобы работать с приложением через графический интерфейс.
 """
 import sys
+from typing import Sequence
 
 from PySide6.QtWidgets import QApplication
 
@@ -10,13 +11,36 @@ from bookkeeper import settings
 from bookkeeper.qtgui.gui import MainWindow
 
 
+class Application:
+    """
+    Класс, содержащий в себе объект приложения Qt и объект главного окна.
+    """
+
+    def __init__(self, argv: Sequence[str] | None = None) -> None:
+        self.app = QApplication(argv)
+        main_font = self.app.font()
+        main_font.setPointSize(settings.PYSIDE6_MAIN_FONT_SIZE)
+        self.app.setFont(main_font)
+        self.main_window = MainWindow()
+        self.main_window.show()
+
+    def show_main_window(self) -> None:
+        """
+        Отображает главное окно
+        """
+        self.main_window.show()
+
+    def exec(self) -> int:
+        """
+        Возвращает код возврата приложения Qt.
+        Этот метод нужно выполнить для запуска приложения Qt.
+        """
+        return self.app.exec()
+
+
 def main() -> None:
-    app = QApplication(sys.argv)
-    main_font = app.font()
-    main_font.setPointSize(settings.PYSIDE6_MAIN_FONT_SIZE)
-    app.setFont(main_font)
-    main_window = MainWindow()
-    main_window.show()
+    app = Application(sys.argv)
+    app.show_main_window()
     sys.exit(app.exec())
 
 
