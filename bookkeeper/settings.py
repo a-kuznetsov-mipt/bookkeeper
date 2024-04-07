2223839e384505da0f687ca8656b7f5607912521
Alexander Kuznetsov <166259762+a-kuznetsov-mipt@users.noreply.github.com>
2024-04-07 20:50:12 +0300

    settings.py: base project directory added

--- bookkeeper/settings.py
+++ bookkeeper/settings.py
@@ -1,6 +1,10 @@
 """
 Файл настроек приложения bookkeeper.
 """
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parents[1]  # Корневая папка проекта.
+
 PYSIDE6_MAIN_WINDOW_GEOMETRY = 200, 100, 750, 790  # Размеры главного окна приложения.
 PYSIDE6_MAIN_WINDOW_TITLE = 'The Bookkeeper App'  # Заголовок главного окна приложения.
 PYSIDE6_MAIN_FONT_SIZE = 14  # Размер шрифта всего приложения.
