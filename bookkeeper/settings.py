4bce0056c10bd1896ee80b6279847985535d76d8
Alexander Kuznetsov <166259762+a-kuznetsov-mipt@users.noreply.github.com>
2024-04-07 20:50:41 +0300

    settings.py: sqlite database file path added

--- bookkeeper/settings.py
+++ bookkeeper/settings.py
@@ -5,6 +5,8 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).parents[1]  # Корневая папка проекта.
 
+SQLITE_DB_FILE_PATH = BASE_DIR / 'bookkeeper' / 'db.sqlite3'  # Путь до базы данных.
+
 PYSIDE6_MAIN_WINDOW_GEOMETRY = 200, 100, 750, 790  # Размеры главного окна приложения.
 PYSIDE6_MAIN_WINDOW_TITLE = 'The Bookkeeper App'  # Заголовок главного окна приложения.
 PYSIDE6_MAIN_FONT_SIZE = 14  # Размер шрифта всего приложения.
