0805f23e14cdda456abe378e9ed5e14b65e72f48
Alexander Kuznetsov <166259762+a-kuznetsov-mipt@users.noreply.github.com>
2024-04-07 03:39:16 +0300

    GUI draft created
    Application is now runnable in gui mode via gui_client.py. All required widget are present but no functionality is yet implemented.

---
+++ bookkeeper/gui_client.py
@@ -0,0 +1,35 @@
+"""
+Основной скрипт проекта, который следует запускать,
+чтобы работать с приложением через графический интерфейс.
+"""
+import sys
+from typing import Sequence
+
+from PySide6.QtWidgets import QApplication
+
+from bookkeeper.qtgui.gui import MainWindow
+
+
+class Application(QApplication):
+    """
+    Класс приложения.
+    В нём происходит создание главного окна приложения.
+    Так же в нём устанавливается размер шрифта для всего приложения,
+    т. к. размер шрифта - один из наиболее важных параметров, влияющих опыт взаимодействия.
+    """
+    def __init__(self, argv: Sequence[str] = None) -> None:
+        super().__init__(argv)
+        main_font = self.font()
+        main_font.setPointSize(14)
+        self.setFont(main_font)
+        self.main_window = MainWindow()
+        self.main_window.show()
+
+
+def main() -> None:
+    app = Application(sys.argv)
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    main()
---
+++ bookkeeper/qtgui/gui.py
@@ -0,0 +1,81 @@
+"""
+В данном модуле будут описаны виджеты, используемые в графическом интерфейсе.
+"""
+
+from typing import Optional
+
+from PySide6.QtWidgets import (
+    QMainWindow,
+    QWidget,
+    QLabel,
+    QGridLayout,
+    QLineEdit,
+    QComboBox,
+    QPushButton,
+    QTableWidget,
+    QHeaderView,
+)
+
+
+class MainWindow(QMainWindow):
+    """
+    Класс главного окна.
+    Здесь настраивается размеры окна и его название,
+    а так же создаётся главный вижет.
+    Ни каких других виджетов, кроме главного,
+    не создаётся в конструкторе этого класса напрямую
+    """
+    def __init__(self, parent: Optional[QWidget] = None) -> None:
+        super().__init__(parent)
+        geometry = 200, 100, 750, 790
+        *_, width, height = geometry
+        self.setGeometry(*geometry)
+        self.setWindowTitle('The Bookkeeper App')
+        self.setCentralWidget(MainWidget())
+        self.setFixedSize(width, height)
+
+
+class MainWidget(QWidget):
+    """
+    Класс главного виджета.
+    Здесь создаётся раскладка (layout) и все вижеты (надписи, таблицы, кнопки и т. д.).
+    """
+    LAYOUT_COLUMN_STRETCHES = [1, 6, 1]
+    LAYOUT_ROW_STRETCHES = [
+        1,
+        7,
+        1,
+        5,
+        1,
+        1,
+        1,
+    ]
+
+    def __init__(self, parent: Optional[QWidget] = None) -> None:
+        super().__init__(parent)
+        self.layout = QGridLayout(self)
+        self.setLayout(self.layout)
+        for column, stretch in enumerate(self.LAYOUT_COLUMN_STRETCHES):
+            self.layout.setColumnStretch(column, stretch)
+        for row, stretch in enumerate(self.LAYOUT_ROW_STRETCHES):
+            self.layout.setRowStretch(row, stretch)
+
+        self.layout.addWidget(QLabel('Последние расходы'), 0, 0, 1, 3)
+        table_last_expenses = QTableWidget(10, 4)
+        table_last_expenses.setHorizontalHeaderLabels(['Дата', 'Сумма', 'Категория', 'Комметарий'])
+        table_last_expenses.horizontalHeader().setStretchLastSection(True)
+        table_last_expenses.verticalHeader().setVisible(False)
+        self.layout.addWidget(table_last_expenses, 1, 0, 1, 3)
+        self.layout.addWidget(QLabel('Бюджет'), 2, 0, 1, 3)
+        table_budget = QTableWidget(3, 2)
+        table_budget.setHorizontalHeaderLabels(['Сумма', 'Бюджет'])
+        table_budget.setVerticalHeaderLabels(['День', 'Неделя', 'Месяц'])
+        table_budget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        table_budget.verticalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.layout.addWidget(table_budget, 3, 0, 1, 3)
+        self.layout.addWidget(QLabel('Сумма'), 4, 0, 1, 1)
+        self.layout.addWidget(QLabel('Категория'), 5, 0, 1, 1)
+        self.layout.addWidget(QLineEdit(), 4, 1, 1, 1)
+        self.layout.addWidget(QComboBox(), 5, 1, 1, 1)
+        self.layout.addWidget(QPushButton('Добавить'), 6, 1, 1, 1)
+        self.layout.addWidget(QPushButton('Редактировать'), 5, 2, 1, 1)
