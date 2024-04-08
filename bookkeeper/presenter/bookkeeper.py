1317d3c134255c0c876161312cb1d877ed6bc956
Alexander Kuznetsov <166259762+a-kuznetsov-mipt@users.noreply.github.com>
2024-04-08 20:03:14 +0300

    Expenses and budget sums calculation implemented in the presenter

--- bookkeeper/presenter/bookkeeper.py
+++ bookkeeper/presenter/bookkeeper.py
@@ -1,3 +1,5 @@
+import datetime
+
 from bookkeeper.models.budget import Budget
 from bookkeeper.models.category import Category
 from bookkeeper.models.expense import Expense
@@ -10,6 +12,12 @@ class BookkeeperPresenter:
     Класс презентера.
     Связующее звено между репозиторием и представлением.
     """
+
+    TIMEDELTA_ZERO = datetime.timedelta(0)
+    TIMEDELTA_DAY = datetime.timedelta(days=1)
+    TIMEDELTA_WEEK = datetime.timedelta(days=7)
+    TIMEDELTA_MONTH = datetime.timedelta(days=30)
+
     def __init__(
             self,
             repository_budgets: AbstractRepository[Budget],
@@ -25,6 +33,8 @@ class BookkeeperPresenter:
             self.repository_budgets.get_all(),
             self.repository_categories.get_all(),
             self.repository_expenses.get_all(),
+            self._calculate_current_budget_sums(),
+            self._calculate_current_expenses_sums()
         )
 
     def run(self) -> None:
@@ -33,3 +43,47 @@ class BookkeeperPresenter:
         Фактически, запускает приложение.
         """
         self.view.run()
+
+    def _calculate_current_budget_sums(self) -> list[int]:
+        """
+        Возвращает суммы бюджетов по категориям за день, месяц, неделю.
+        """
+        budgets = self.repository_budgets.get_all()
+        budgets_sum_dayly = sum([budget.amount
+                                 for budget in budgets
+                                 if budget.period == 'день'])
+        budgets_sum_weeky = sum([budget.amount
+                                 for budget in budgets
+                                 if budget.period == 'неделя'])
+        budgets_sum_monthly = sum([budget.amount
+                                   for budget in budgets
+                                   if budget.period == 'месяц'])
+        return [
+            budgets_sum_dayly,
+            budgets_sum_weeky,
+            budgets_sum_monthly,
+        ]
+
+    def _calculate_current_expenses_sums(self) -> list[int]:
+        """
+        Возвращает суммы расходов по категориям за день, месяц, неделю.
+        """
+        expenses = self.repository_expenses.get_all()
+        expenses_sum_dayly = 0
+        expenses_sum_weeky = 0
+        expenses_sum_monthly = 0
+        now = datetime.datetime.now()
+        for expense in expenses:
+            now_to_expense_datetime_timedelta = now - expense.expense_date
+            if now_to_expense_datetime_timedelta > self.TIMEDELTA_ZERO:
+                if now_to_expense_datetime_timedelta < self.TIMEDELTA_DAY:
+                    expenses_sum_dayly += expense.amount
+                if now_to_expense_datetime_timedelta < self.TIMEDELTA_WEEK:
+                    expenses_sum_weeky += expense.amount
+                if now_to_expense_datetime_timedelta < self.TIMEDELTA_MONTH:
+                    expenses_sum_monthly += expense.amount
+        return [
+            expenses_sum_dayly,
+            expenses_sum_weeky,
+            expenses_sum_monthly,
+        ]
