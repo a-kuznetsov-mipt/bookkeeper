950018be0f406ae800674b4842248af20508de41
Alexander Kuznetsov <166259762+a-kuznetsov-mipt@users.noreply.github.com>
2024-04-08 22:53:40 +0300

    Window geometry changed in settings

--- bookkeeper/settings.py
+++ bookkeeper/settings.py
@@ -7,6 +7,6 @@ BASE_DIR = Path(__file__).parents[1]  # Корневая папка проект
 
 SQLITE_DB_FILE_PATH = BASE_DIR / 'bookkeeper' / 'db.sqlite3'  # Путь до базы данных.
 
-PYSIDE6_MAIN_WINDOW_GEOMETRY = 200, 100, 750, 790  # Размеры главного окна приложения.
+PYSIDE6_MAIN_WINDOW_GEOMETRY = 200, 100, 900, 790  # Размеры главного окна приложения.
 PYSIDE6_MAIN_WINDOW_TITLE = 'The Bookkeeper App'  # Заголовок главного окна приложения.
 PYSIDE6_MAIN_FONT_SIZE = 14  # Размер шрифта всего приложения.
